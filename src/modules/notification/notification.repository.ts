60eec475e7170d68ce8311db347e10a01cfcff04
20120507 <103519164+khanhvo9484@users.noreply.github.com>
2024-01-29 20:45:37 +0700

    change build command

--- src/modules/notification/notification.repository.ts
+++ src/modules/notification/notification.repository.ts
@@ -1,20 +1,20 @@
-import { Injectable, Inject } from '@nestjs/common'
-import { PrismaService } from '@my-prisma/prisma.service'
-import { Prisma } from '@prisma/client'
+import { Injectable, Inject } from '@nestjs/common';
+import { PrismaService } from '@my-prisma/prisma.service';
+import { Prisma } from '@prisma/client';
 
 @Injectable()
 export class NotificationRepository {
-	constructor(private prismaService: PrismaService) {}
+  constructor(private prismaService: PrismaService) {}
 
-	async updateNotification(params: {
-		where: Prisma.NotificationWhereInput
-		data: Prisma.NotificationUpdateInput
-	}) {
-		const { where, data } = params
-		const result = await this.prismaService.notification.updateMany({
-			where,
-			data
-		})
-		return result
-	}
+  async updateNotification(params: {
+    where: Prisma.NotificationWhereInput;
+    data: Prisma.NotificationUpdateInput;
+  }) {
+    const { where, data } = params;
+    const result = await this.prismaService.notification.updateMany({
+      where,
+      data,
+    });
+    return result;
+  }
 }
